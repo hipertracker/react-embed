909564598e4c643ec931866f4b77b07d811f5eca
Jaroslaw Zabiello <hipertracker@gmail.com>
2015-03-23 09:43:39 +0000

    added polyfill

--- requirejs-config.js
+++ requirejs-config.js
@@ -2,20 +2,16 @@ var require = {
     waitSeconds: 200,
     baseUrl: './',
     paths: {
-        babel: 'vendor/requirejs_babel/babel-4.7.8.min',
-        babel_polyfill: 'vendor/requirejs_babel/browser-polyfill',
+        babel: 'vendor/requirejs_babel/babel-4.7.12.min',
+        babel_polyfill: 'vendor/requirejs_babel/polyfill-4.7.12.min',
         es6: 'vendor/requirejs_babel/es6',
-
-        // for stupid IE9
-        'es5-shim': 'vendor/es5-shim/es5-shim.min',
-        'es5-sham': 'vendor/es5-shim/es5-sham.min',
 
         react: 'vendor/react/react-with-addons',
         app: './app'
     },
     shim: {
         babel: {
-            deps: ['babel_polyfill', 'es5-shim', 'es5-sham']
+            deps: ['babel_polyfill']
         }
     }
 };
